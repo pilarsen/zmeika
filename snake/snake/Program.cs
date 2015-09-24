8df89136d56ae93386145ebc7a5446222ac6281f
pilarsen <piloyanarsen97@gmail.com>
2015-09-24 22:15:48 +0300

    Add Draw Function

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,9 +10,28 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            int x1 = 15;
+            int y1 = 10;
+            char sym1 = '*';
+
+
+            int x2 = 4;
+            int y2 = 7;
+            char sym2 = '#';
+
+            Draw(x1, y1, sym1);
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
 
         }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    
     }
+
 }
