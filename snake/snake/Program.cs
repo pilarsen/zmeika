c318fb4e4845915234b53f180a59da7c1c321e5d
pilarsen <piloyanarsen97@gmail.com>
2015-09-24 22:59:38 +0300

    Create Class Point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,19 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 15;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 5;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 15;
+            p2.sym = '$';
+            p2.Draw();
+
+            /*int x1 = 15;
             int y1 = 10;
             char sym1 = '*';
 
@@ -20,17 +32,12 @@ namespace snake
             char sym2 = '#';
 
             Draw(x1, y1, sym1);
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
 
             Console.ReadLine();
 
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
     
     }
 
