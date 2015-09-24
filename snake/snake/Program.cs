b043aab0c87523e85c0f6bf3635a67f3bcc9c2b7
pilarsen <piloyanarsen97@gmail.com>
2015-09-24 23:27:05 +0300

    Create Const f Method Class

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,30 +10,11 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 5;
-            p1.sym = '*';
+            Point p1 = new Point(1, 5, '*');
             p1.Draw();
-
-            Point p2 = new Point();
-            p2.x = 5;
-            p2.y = 15;
-            p2.sym = '$';
+            Point p2 = new Point(5, 15, '$');
             p2.Draw();
-
-            /*int x1 = 15;
-            int y1 = 10;
-            char sym1 = '*';
-
-
-            int x2 = 4;
-            int y2 = 7;
-            char sym2 = '#';
-
-            Draw(x1, y1, sym1);
-            Draw(x2, y2, sym2);*/
-
+            
             Console.ReadLine();
 
         }
